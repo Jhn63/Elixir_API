d6d837897975a83efa54836ad55e9e1ce1c0ffaa
Jorge Alberto Oliveira <sieghart1982@gmail.com>
2024-09-08 00:33:28 -0300

    NEW: add MagicModule querys

--- lib/game.ex
+++ lib/game.ex
@@ -3,19 +3,77 @@ defmodule Game do
   Módulo que implementa o jogo de adivinhar a carta usando o módulo Magic.
   """
 
-  def start_game() do
+  def start() do
     IO.puts("Bem-vindo ao jogo de adivinhar a carta!")
-    cartas = Magic.mostrar_cartas()
+
+    # Escolha se o jogador quer adivinhar por cor ou por tipo
+    IO.puts("Você quer adivinhar cartas por identidade de cor ou tipo?")
+    IO.puts("Digite 1 para cor ou 2 para tipo")
+    opcao = IO.gets("> ") |> String.trim()
+
+    case opcao do
+      "1" ->
+        iniciar_jogo_por_cor()
+
+      "2" ->
+        iniciar_jogo_por_tipo()
+
+      _ ->
+        IO.puts("Opção inválida! Tente novamente.")
+        # Reinicia o jogo se a opção for inválida
+        start()
+    end
+  end
+
+  defp iniciar_jogo_por_cor() do
+    IO.puts("Qual identidade de cor você quer? Escolha apenas uma. Exemplo: Red, Blue, Green")
+
+    cor =
+      IO.gets("> ")
+      |> String.trim()
+      |> Magic.get_color()
+
+    # Pegando cartas filtradas por cor
+    cartas = Magic.get_cards([%{param: "colors", term: cor}])
 
     if length(cartas) == 0 do
-      IO.puts("Nenhuma carta encontrada.")
+      IO.puts("Nenhuma carta com a cor #{cor} encontrada.")
+      start()
     else
-      carta_secreta = escolher_carta_aleatoria(cartas)
-      IO.puts("Eu escolhi uma carta. Tente adivinhar!")
-      IO.puts(carta_secreta["name"])
-
-      loop_adivinhar(carta_secreta)
+      iniciar_jogo_com_cartas(cartas)
     end
+  end
+
+  defp iniciar_jogo_por_tipo() do
+    IO.puts("Qual tipo de carta você quer? Se desejar ver todos os tipos insira 0.")
+    tipo = IO.gets("> ") |> String.trim()
+
+    tipo =
+      if tipo == "0" do
+        IO.inspect(Magic.get_types())
+        IO.gets("> ") |> String.trim()
+      else
+        tipo
+      end
+
+    # Pegando cartas filtradas por tipo
+    cartas = Magic.get_cards([%{param: "types", term: tipo}])
+
+    if length(cartas) == 0 do
+      IO.puts("Nenhuma carta com o tipo #{tipo} encontrada.")
+      start()
+    else
+      iniciar_jogo_com_cartas(cartas)
+    end
+  end
+
+  defp iniciar_jogo_com_cartas(cartas) do
+    carta_secreta = escolher_carta_aleatoria(cartas)
+    IO.puts("Eu escolhi uma carta. Tente adivinhar!")
+    # Pode remover isso, está aqui apenas para facilitar testes
+    IO.puts(carta_secreta["name"])
+
+    loop_adivinhar(carta_secreta)
   end
 
   defp escolher_carta_aleatoria(cartas) do
@@ -40,11 +98,11 @@ defmodule Game do
       fn -> "Tipo da carta: #{carta["type"]}" end,
       fn -> "Custo de mana: #{carta["manaCost"]}" end,
       fn -> "Poder: #{carta["power"] || "N/A"}" end,
-      fn -> "Resistência: #{carta["toughness"] || "N/A"}" end
+      fn -> "Resistência: #{carta["toughness"] || "N/A"}" end,
+      fn -> "Descrição: #{carta["description"] || carta["text"]}" end
     ]
 
     dica_aleatoria = Enum.random(dicas)
-    # Garantir que a função seja chamada corretamente
     IO.puts(dica_aleatoria.())
   end
 end
--- lib/magic.ex
+++ lib/magic.ex
@@ -1,61 +1,84 @@
 defmodule Magic do
   @cards_url "https://api.magicthegathering.io/v1/cards"
-  @sets_url "https://api.magicthegathering.io/v1/sets"
   @types_url "https://api.magicthegathering.io/v1/types"
-  @subtypes_url "https://api.magicthegathering.io/v1/subtypes"
-  @supertypes_url "https://api.magicthegathering.io/v1/supertypes"
+  @color_identity %{
+    "White" => "W",
+    "Blue" => "U",
+    "Black" => "B",
+    "Red" => "R",
+    "Green" => "G"
+  }
+
+  def teste() do
+    Magic.get_cards([
+      %{param: "language", term: "Portuguese (Brazil)"},
+      %{param: "type", term: "Creature"},
+      %{param: "pageSize", term: "20"}
+      # %{param: "name", term: "Não É PRA VIR NADA"}
+    ])
+  end
 
   def get_cards() do
-    data = Api.get(@cards_url)
+    @cards_url
+    |> Api.get()
+    |> format_data()
+  end
+
+  def get_color(color) do
+    @color_identity
+    |> Map.get(String.capitalize(color), "Cor inválida")
   end
 
   def get_types() do
-    data = Api.get(@types_url)
+    @types_url
+    |> Api.get()
+    |> Map.get("types")
   end
 
   def get_cards(params) when is_list(params) do
-    data =
-      Api.build_url(@cards_url, params)
-      |> Api.get()
+    @cards_url
+    |> Api.build_url(params)
+    |> Api.get()
+    |> format_data(params)
   end
 
-  @spec mostrar_cartas(String.t(), String.t()) :: [map()]
-  def mostrar_cartas(card_name, language) do
-    data = buscar_cartas_por_idioma(card_name, language)
-    mostrar_cartas(data["cards"], "", language)
-  end
+  defp format_data(data), do: data["cards"]
 
-  defp mostrar_cartas([], _type, _language), do: []
+  defp format_data(data, []), do: data["cards"]
 
-  defp mostrar_cartas([i | resto], type, language) do
-    if type == "" or String.contains?(i["type"], type) do
-      carta = Map.take(i, ["name", "manaCost", "type", "power", "toughness", "foreignNames"])
+  defp format_data(data, params) do
+    formats = %{
+      "language" => &format_language/2
+    }
 
-      carta =
-        if language != "" do
-          carta_descricao = buscar_descricao(carta, language)
-          Map.put(carta, "description", carta_descricao)
-        else
-          carta
+    Enum.map(data["cards"], fn card ->
+      Enum.reduce(params, card, fn map, acc_card ->
+        param_value = Map.get(map, :param)
+        term_value = Map.get(map, :term)
+
+        case Map.get(formats, param_value) do
+          nil -> acc_card
+          format_func -> format_func.(acc_card, term_value)
         end
-
-      carta = Map.drop(carta, ["foreignNames"])
-      [carta | mostrar_cartas(resto, type, language)]
-    else
-      mostrar_cartas(resto, type, language)
-    end
+      end)
+    end)
   end
 
-  defp buscar_cartas_por_idioma(name, language) do
-    url = "#{@cards_url}?name=#{name}&language=#{language}"
-    Api.get(url)
+  defp format_language(card, language) do
+    description = get_description(card, language)
+
+    card
+    |> Map.delete("foreignNames")
+    |> Map.put("description", description)
   end
 
-  defp buscar_descricao(carta, language) do
-    carta["foreignNames"]
+  defp get_description(card, language) do
+    foreign_names = Map.get(card, "foreignNames", [])
+
+    foreign_names
     |> Enum.find(fn f -> f["language"] == language end)
     |> case do
-      nil -> "Descrição em português não encontrada."
+      nil -> "Descrição em #{language} não encontrada."
       desc -> desc["text"]
     end
   end
